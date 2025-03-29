e659547cf4c77975b713ad179f6c39b0a71ccf87
Dmitry <dmitryryabickov@yandex.ru>
2025-03-29 13:41:32 +0300

    upd

--- rtl/axis_spi_top.sv
+++ rtl/axis_spi_top.sv
@@ -10,17 +10,21 @@ module axis_spi_top #(
     parameter MEM_DEPTH    = 24,
     parameter MEM_WIDTH    = 16
 ) (
-    input  logic clk_i,
-    input  logic arstn_i,
+    input  logic                 clk_i,
+    input  logic                 arstn_i,
 
-    output logic spi_clk_o,
-    output logic spi_cs_o,
-    output logic spi_mosi_o,
-    input  logic spi_miso_i
+    output logic                 spi_clk_o,
+    output logic [SLAVE_NUM-1:0] spi_cs_o,
+    output logic                 spi_mosi_o,
+    input  logic                 spi_miso_i
 );
 
 axis_if s_axis();
 axis_if m_axis();
+
+logic [$clog2(SLAVE_NUM)-1:0] addr_i;
+
+assign addr_i = '0;
 
 axis_spi_master #(
     .SPI_MODE   (SPI_MODE  ),
@@ -32,7 +36,7 @@ axis_spi_master #(
 ) i_axis_spi_master (
     .clk_i      (clk_i     ),
     .arstn_i    (arstn_i   ),
-    .addr_i     ('0        ),
+    .addr_i     (addr_i    ),
     .spi_clk_o  (spi_clk_o ),
     .spi_cs_o   (spi_cs_o  ),
     .spi_mosi_o (spi_mosi_o),

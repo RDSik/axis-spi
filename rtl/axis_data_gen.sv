b93cbddc15317ccb88c5000e9bbf00464b7a1cf5
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-04-19 01:14:19 +0300

    Update axis_data_gen.sv

--- rtl/axis_data_gen.sv
+++ rtl/axis_data_gen.sv
@@ -3,16 +3,18 @@ module axis_data_gen #(
     parameter CONFIG_MEM = "rtl/config.mem",
     parameter MEM_DEPTH  = 24,
     parameter MEM_WIDTH  = 8
-)(
+) (
     input logic clk_i,
     input logic arstn_i,
 
     axis_if m_axis
 );
 
-logic [$clog2(MEM_DEPTH)-1:0] index;
-
 logic [MEM_WIDTH-1:0] config_mem [0:MEM_DEPTH-1];
+
+logic [$clog2(MEM_DEPTH)-1:0] index;
+logic                         index_done;
+logic                         m_handshake;
 
 initial begin
     $readmemh(CONFIG_MEM, config_mem);
@@ -21,20 +23,26 @@ end
 always_ff @(posedge clk_i or negedge arstn_i) begin
     if (~arstn_i) begin
         index <= '0;
-    end else if (m_axis.tvalid & m_axis.tready) begin
-        index <= (index == MEM_DEPTH - 1) ? '0 : index + 1'b1;
+    end else if (m_handshake) begin
+        index <= index_done ? '0 : index + 1'b1;
     end
 end
 
 always_ff @(posedge clk_i or negedge arstn_i) begin
     if (~arstn_i) begin
         m_axis.tvalid <= 1'b0;
+        m_axis.tlast  <= 1'b0;
+    end else if (m_handshake) begin
+        m_axis.tvalid <= 1'b0;
+        m_axis.tlast  <= 1'b0;
     end else begin
         m_axis.tvalid <= 1'b1;
+        m_axis.tlast  <= (index_done) ? 1'b0 : 1'b1;
     end
 end
 
-assign m_axis.tlast = (index == MEM_DEPTH - 1) ? 1'b0 : 1'b1;
+assign index_done   = (index == MEM_DEPTH - 1) ? 1'b0 : 1'b1
+assign m_handshake  = m_axis.tvalid & m_axis.tready;
 assign m_axis.tdata = config_mem[index];
 
 endmodule
